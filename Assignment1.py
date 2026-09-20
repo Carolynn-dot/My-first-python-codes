d4ef9a2537b766e32975d93a95099df6efb07403
Carolynn-dot <caromollel03@gmail.com>
2026-09-20 16:15:45 +0300

    Add basic grading program in Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,8 @@
+#My first program - Caroline @ CoICT
+jina = input("Jina lako: ")
+alama = int(input("Ingiza alama: "))
+
+if alama >= 40:
+    print(f"Hongera {jina}, umepita!")
+else:
+    print(f"Pole {jina}, umefeli")
